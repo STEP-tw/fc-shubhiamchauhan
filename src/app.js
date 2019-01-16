291f0c9243ac60d861a203cdd1c10bc58fb89cf7
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2019-01-16 12:32:15 +0530

    Made server to give response on valid request.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
-const app = (req, res) => {
-  res.statusCode = 404;
+const fs = require("fs");
+
+const send = function(res, statusCode, content) {
+  res.statusCode = statusCode;
+  res.write(content);
   res.end();
+};
+
+const app = (req, res) => {
+  if (req.url == "/favicon.ico") {
+    res.end();
+    return;
+  }
+  fs.readFile("." + req.url, (err, chunk) => {
+    send(res, 200, chunk);
+  });
+
 };
 
 // Export a function that can act as a handler
